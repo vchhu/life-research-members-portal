9ea820b013bb4964681bc582b65bb4eed31669b1
Chris Hamilton <chris_h_27@hotmail.ca>
2022-09-22 17:55:48 -0400

    add doc link

--- src/utils/auth-header.ts
+++ src/utils/auth-header.ts
@@ -1,3 +1,5 @@
+// See https://learn.microsoft.com/en-us/azure/active-directory/develop/userinfo
+
 export default function authHeader(accessToken: string) {
   return { authorization: "Bearer " + accessToken };
 }
