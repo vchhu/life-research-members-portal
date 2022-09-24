52a8ee2c4428bf0465230b3b0689d74f8dd3dc84
Chris Hamilton <chris_h_27@hotmail.ca>
2022-09-24 12:37:41 -0400

    clean up register user

--- src/pages/api/register-user.ts
+++ src/pages/api/register-user.ts
@@ -7,21 +7,20 @@ import getUser from "../../utils/api/get-user";
 type Data = any;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  if (!req.body.email) return res.status(400).send("No email found in request body.");
+  const { email, admin, microsoft_sub } = req.body;
+  if (!email) return res.status(400).send("Email is required.");
   try {
-    const user = await getUser(req, res);
-    if (!user) return;
-    if (!user.admin) {
+    const currentUser = await getUser(req, res);
+    if (!currentUser) return;
+    if (!currentUser.admin)
       return res.status(401).send("You are not authorized to perform this action.");
-    }
-    const test = await prisma.auth_Users.create({
-      data: { email: req.body.email, admin: req.body.admin, microsoft_sub: req.body.microsoft_sub },
+    const newUser = await prisma.auth_Users.create({
+      data: { email, admin, microsoft_sub },
     });
-    res.status(200).send("User created: " + JSON.stringify(test));
+    res.status(200).send("User created: " + JSON.stringify(newUser));
   } catch (e: any) {
     if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2002")
-      res.status(400).send("This email is already registered. Email: " + req.body.email);
-    // prisma error messages are getters
-    res.status(500).send({ error: e, message: e.message });
+      res.status(400).send("This email is already registered. Email: " + email);
+    res.status(500).send({ error: e, message: e.message }); // prisma error messages are getters
   }
 }
