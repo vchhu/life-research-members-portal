ef8185360a5447d3d2598e85b02759dbff5fe1b2
Chris Hamilton <chris_h_27@hotmail.ca>
2022-09-26 13:19:41 -0400

    Add links to view and edit members

--- src/components/all-members.tsx
+++ src/components/all-members.tsx
@@ -1,26 +1,46 @@
-import { FunctionComponent, useState } from "react";
+import { main_Members } from "@prisma/client";
+import Link from "next/link";
+import { Fragment, FunctionComponent, useEffect, useState } from "react";
 import ApiRoutes from "../utils/front-end/api-routes";
 import authHeader from "../utils/front-end/auth-header";
+import PageRoutes from "../utils/front-end/page-routes";
+
+let firstRender = true;
+let cachedMembers: main_Members[] = [];
 
 const AllMembers: FunctionComponent = () => {
-  const [_allMembers, setAllMembers] = useState<{ ID: number }[]>([]);
-  const allMembers = _allMembers.map((member) => (
-    <pre key={member.ID}>{JSON.stringify(member, null, 2)}</pre>
+  const [allMembers, setAllMembers] = useState<main_Members[]>(cachedMembers);
+  const allMembersHtml = allMembers.map((member) => (
+    <Fragment key={member.ID}>
+      <pre>{JSON.stringify(member, null, 2)}</pre>
+      <Link href={PageRoutes.viewMember + member.ID}>
+        <button>VIEW</button>
+      </Link>
+      <span style={{ width: "6px", display: "inline-block" }}></span>
+      <Link href={PageRoutes.editMember + member.ID}>
+        <button>EDIT</button>
+      </Link>
+    </Fragment>
   ));
 
   async function fetchAllMembers() {
     const result = await fetch(ApiRoutes.allMembers, { headers: await authHeader() });
     if (!result.ok) return console.error(await result.text());
-    setAllMembers(await result.json());
+    const members = await result.json();
+    setAllMembers(members);
+    cachedMembers = members;
   }
+
+  useEffect(() => {
+    if (firstRender) fetchAllMembers();
+    firstRender = false;
+  }, []);
 
   return (
     <>
       <h1>All Members</h1>
-      <button onClick={fetchAllMembers}>Get All Members</button>
-      <br />
-      <button onClick={() => setAllMembers([])}>Clear</button>
-      {allMembers}
+      <button onClick={fetchAllMembers}>REFRESH</button>
+      {allMembersHtml}
     </>
   );
 };
--- src/utils/front-end/page-routes.ts
+++ src/utils/front-end/page-routes.ts
@@ -2,6 +2,8 @@ const PageRoutes = {
   home: "/",
   members: "/members",
   register: "/register",
+  viewMember: "/view-member/",
+  editMember: "/edit-member/",
 } as const;
 
 export default PageRoutes;
