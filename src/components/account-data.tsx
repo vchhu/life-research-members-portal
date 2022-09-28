30ba3483b93ff13c7d17f283ab10ac0dbb458c44
Chris Hamilton <chris_h_27@hotmail.ca>
2022-09-28 00:43:35 -0400

    notify if account is not registered

--- src/components/account-data.tsx
+++ src/components/account-data.tsx
@@ -4,15 +4,18 @@ import React, { FunctionComponent, useEffect, useState } from "react";
 import ApiRoutes from "../utils/front-end/api-routes";
 import authHeader from "../utils/front-end/auth-header";
 
+let firstRender = true;
 let cachedLocalAccount: auth_accounts | null = null;
 
 const AccountData: FunctionComponent = () => {
   const { instance } = useMsal();
   const user = instance.getActiveAccount();
   const [localAccount, setLocalAccount] = useState(cachedLocalAccount);
+  const [loading, setLoading] = useState(false);
 
   async function fetchLocalAccount() {
     try {
+      setLoading(true);
       const accountRes = await fetch(ApiRoutes.activeAccount, { headers: await authHeader() });
       if (!accountRes.ok) return console.error(await accountRes.text());
       const account = await accountRes.json();
@@ -20,15 +23,21 @@ const AccountData: FunctionComponent = () => {
       cachedLocalAccount = account;
     } catch (e) {
       console.error(e);
+    } finally {
+      setLoading(false);
     }
   }
 
   useEffect(() => {
-    if (!cachedLocalAccount) fetchLocalAccount();
-  }, []);
+    if (firstRender && user) {
+      fetchLocalAccount();
+      firstRender = false;
+    }
+  }, [user]);
 
   function localAccountData() {
-    if (!localAccount) return <p>Loading...</p>;
+    if (loading) return <p>Loading...</p>;
+    if (!localAccount) return <p>Account is not registered.</p>;
     return (
       <>
         <p>{localAccount.microsoft_email}</p>
