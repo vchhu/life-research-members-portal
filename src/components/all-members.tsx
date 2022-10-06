91e1e8b122edcf417f6e0de8c14c0e7ae1e29d05
Chris Hamilton <chris_h_27@hotmail.ca>
2022-10-06 06:54:05 -0400

    Improve members table

--- src/components/all-members.tsx
+++ src/components/all-members.tsx
@@ -1,24 +1,38 @@
 import { main_members } from "@prisma/client";
+import Button from "antd/lib/button";
 import Table, { ColumnType } from "antd/lib/table";
+import Title from "antd/lib/typography/Title";
 import { FunctionComponent } from "react";
 import useAllMembers from "../utils/front-end/api-facade/use-all-members";
 
 const AllMembers: FunctionComponent = () => {
   const { allMembers, loading, refresh } = useAllMembers();
   const keyedMembers = allMembers.map((m) => ({ ...m, key: m.id }));
+  console.log(allMembers);
 
   const columns: ColumnType<main_members>[] = [
     { title: "First Name", dataIndex: "first_name" },
     { title: "Last Name", dataIndex: "last_name" },
+    { title: "Email", dataIndex: "email" },
+    { title: "Phone (Business)", dataIndex: "business_phone" },
+    { title: "Phone (Mobile)", dataIndex: "mobile_phone" },
+    { title: "Faculty", dataIndex: ["types_faculty", "faculty_name_en"] },
+    { title: "Member Type", dataIndex: ["types_member_category", "category_name_en"] },
   ];
 
-  return (
-    <>
-      <h1>All Members</h1>
-      <button onClick={refresh}>REFRESH</button>
-      <Table columns={columns} dataSource={keyedMembers} loading={loading} />
-    </>
+  const header = () => (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <Title level={1} style={{ margin: 0 }}>
+        All Members
+      </Title>
+      <div style={{ flexGrow: 15 }}></div>
+      <Button type="primary" onClick={refresh} size="large" style={{ flexGrow: 1 }}>
+        Refresh
+      </Button>
+    </div>
   );
+
+  return <Table columns={columns} dataSource={keyedMembers} loading={loading} title={header} />;
 };
 
 export default AllMembers;
