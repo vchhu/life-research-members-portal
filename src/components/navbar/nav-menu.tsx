ff1fe685373afc752342aec07ab43564a9053df6
Chris Hamilton <chris_h_27@hotmail.ca>
2022-10-05 22:40:01 -0400

    test heroku error 2

--- src/components/navbar/nav-menu.tsx
+++ src/components/navbar/nav-menu.tsx
@@ -11,47 +11,47 @@ import { LocalAccountCtx } from "../../context/local-account-ctx";
 import PageRoutes from "../../utils/front-end/page-routes";
 
 const NavMenu: FunctionComponent = () => {
-  // const { localAccount, loading } = useContext(LocalAccountCtx);
+  const { localAccount, loading } = useContext(LocalAccountCtx);
 
-  // // Everyone
-  // const generalItems = [{ label: "Members", href: PageRoutes.members }];
+  // Everyone
+  const generalItems = [{ label: "Members", href: PageRoutes.members }];
 
-  // // Registered Acounts
-  // const registeredItems = [
-  //   { label: "My Profile", href: PageRoutes.viewAccount + localAccount?.id },
-  // ];
+  // Registered Acounts
+  const registeredItems = [
+    { label: "My Profile", href: PageRoutes.viewAccount + localAccount?.id },
+  ];
 
-  // // Admins
-  // const adminItems = [
-  //   { label: "Accounts", href: PageRoutes.accounts },
-  //   { label: "Register", href: PageRoutes.register },
-  // ];
+  // Admins
+  const adminItems = [
+    { label: "Accounts", href: PageRoutes.accounts },
+    { label: "Register", href: PageRoutes.register },
+  ];
 
-  // const items: { label: string; href: string }[] = [...generalItems];
-  // if (!loading) {
-  //   if (localAccount) for (const it of registeredItems) items.push(it);
-  //   if (localAccount?.is_admin) for (const it of adminItems) items.push(it);
-  // }
+  const items: { label: string; href: string }[] = [...generalItems];
+  if (!loading) {
+    if (localAccount) for (const it of registeredItems) items.push(it);
+    if (localAccount?.is_admin) for (const it of adminItems) items.push(it);
+  }
 
-  // const menuItems: MenuItemType[] = items.map((it) => ({
-  //   label: (
-  //     <Link href={it.href}>
-  //       <a>{it.label}</a>
-  //     </Link>
-  //   ),
-  //   key: it.label,
-  // }));
+  const menuItems: MenuItemType[] = items.map((it) => ({
+    label: (
+      <Link href={it.href}>
+        <a>{it.label}</a>
+      </Link>
+    ),
+    key: it.label,
+  }));
 
-  // if (loading) menuItems.push({ label: <Spin />, key: "loading" });
+  if (loading) menuItems.push({ label: <Spin />, key: "loading" });
 
   return (
     <div className="nav-menu">
-      {/* <Menu
+      <Menu
         items={menuItems}
         mode="horizontal"
         overflowedIndicator={<MenuOutlined className="collapsed-icon" />}
         style={{ fontSize: "inherit" }}
-      /> */}
+      />
     </div>
   );
 };
