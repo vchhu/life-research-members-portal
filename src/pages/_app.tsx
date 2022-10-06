89f04ac0ea4450dfe3c2b66127e18087d3422d46
Chris Hamilton <chris_h_27@hotmail.ca>
2022-10-05 23:40:43 -0400

    make dynamic title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,31 @@ import { msalInstance } from "../../auth-config";
 import Head from "next/head";
 import Navbar from "../components/navbar/@navbar";
 import { LocalAccountCtxProvider } from "../context/local-account-ctx";
+import { useRouter } from "next/router";
+import PageRoutes from "../utils/front-end/page-routes";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  function getSuffix() {
+    const path = router.pathname;
+    // Order matters!
+    if (path.startsWith(PageRoutes.accounts)) return "Accounts";
+    if (path.startsWith(PageRoutes.editAccount)) return "Edit Account";
+    if (path.startsWith(PageRoutes.editMember)) return "Edit Member";
+    if (path.startsWith(PageRoutes.members)) return "Members";
+    if (path.startsWith(PageRoutes.register)) return "Register";
+    if (path.startsWith(PageRoutes.viewAccount)) return "View Account";
+    if (path.startsWith(PageRoutes.viewMember)) return "View Member";
+    if (path.startsWith(PageRoutes.home)) return "Home";
+    return "";
+  }
+  let suffix = getSuffix();
+  if (suffix) suffix = " - " + suffix;
+
   return (
     <>
       <Head>
-        <title>WIIM</title>
+        <title>LIFE{suffix}</title>
       </Head>
       <MsalProvider instance={msalInstance}>
         <LocalAccountCtxProvider>
