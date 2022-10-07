a0237d5ab1d246fe10f1fb0f84cf07a72c49e577
Chris Hamilton <chris_h_27@hotmail.ca>
2022-10-07 08:35:43 -0400

    fix auth guard to stop flash of text

--- src/components/auth-guard/auth-guard.tsx
+++ src/components/auth-guard/auth-guard.tsx
@@ -11,20 +11,11 @@ type Props = {
 };
 
 const AuthGuard: FunctionComponent<PropsWithChildren<Props>> = ({ auths, id, children }) => {
-  const router = useRouter();
   const { localAccount, loading } = useContext(AccountCtx);
-  const [authorized, setAuthorized] = useState(false);
-
-  useEffect(() => {
-    if (loading) return;
-    if (auths.includes(Authorizations.admin) && localAccount?.is_admin) return setAuthorized(true);
-    if (auths.includes(Authorizations.registered) && localAccount) return setAuthorized(true);
-    if (auths.includes(Authorizations.matchId) && localAccount?.id === id)
-      return setAuthorized(true);
-  }, [loading, auths, localAccount, id, router]);
-
   if (loading) return <CenteredSpinner />;
-  if (authorized) return <>{children}</>;
+  if (auths.includes(Authorizations.admin) && localAccount?.is_admin) return <>{children}</>;
+  if (auths.includes(Authorizations.registered) && localAccount) return <>{children}</>;
+  if (auths.includes(Authorizations.matchId) && localAccount?.id === id) return <>{children}</>;
   return <h1 style={{ textAlign: "center" }}>You are not authorized to view this page.</h1>;
 };
 
