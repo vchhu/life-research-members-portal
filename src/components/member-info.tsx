baacf32479b97d2eec278de6a8d4f594ce4067fd
Chris Hamilton <chris_h_27@hotmail.ca>
2022-10-10 18:54:44 -0400

    style member info

--- src/components/member-info.tsx
+++ src/components/member-info.tsx
@@ -1,10 +1,14 @@
+import Grid from "antd/lib/grid";
+import Button from "antd/lib/button";
 import Card from "antd/lib/card";
 import Descriptions from "antd/lib/descriptions";
 import Item from "antd/lib/descriptions/Item";
-import Title from "antd/lib/typography/Title";
 import { FunctionComponent } from "react";
 import { all_member_info } from "../../prisma/types";
 import MemberInfoSkeleton from "./loading/member-info-skeleton";
+import Title from "antd/lib/typography/Title";
+
+const { useBreakpoint } = Grid;
 
 type Props = {
   editable: boolean;
@@ -12,11 +16,13 @@ type Props = {
 };
 
 const MemberInfo: FunctionComponent<Props> = ({ editable, member }) => {
+  const screens = useBreakpoint();
+
   if (!member) return <MemberInfoSkeleton />;
 
-  let title = "";
-  if (!member.first_name || !member.last_name) title = "Member " + member.id;
-  else title = (member.first_name || "") + " " + (member.last_name || "");
+  let titleText = "";
+  if (!member.first_name || !member.last_name) titleText = "Member " + member.id;
+  else titleText = (member.first_name || "") + " " + (member.last_name || "");
 
   let address = "";
   if (member.address) address += member.address;
@@ -48,9 +54,45 @@ const MemberInfo: FunctionComponent<Props> = ({ editable, member }) => {
       " / " +
       member.types_member_category?.category_name_fr;
 
+  const editButton = () => {
+    if (!editable) return null;
+    return (
+      <Button size="large" type="primary" style={{ flexGrow: 1, maxWidth: "10rem" }}>
+        Edit
+      </Button>
+    );
+  };
+
+  const title = (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <Title
+        level={2}
+        style={{
+          margin: 0,
+          // overflow: "hidden",
+          // textOverflow: "ellipsis",
+          minWidth: 0,
+          marginRight: "auto",
+          paddingRight: 16,
+          whiteSpace: "break-spaces",
+        }}
+      >
+        {titleText}
+      </Title>
+      {editButton()}
+    </div>
+  );
+
   return (
-    <Card title={<Title>{title}</Title>} bodyStyle={{ padding: 0 }}>
-      <Descriptions bordered column={1} labelStyle={{ whiteSpace: "nowrap" }}>
+    <Card title={title} bodyStyle={{ padding: 0 }}>
+      <Descriptions
+        size="small"
+        bordered
+        column={1}
+        labelStyle={{ whiteSpace: "nowrap" }}
+        layout={screens.xs ? "vertical" : "horizontal"}
+      >
+        <Item label="Business Name">{member.business_name}</Item>
         <Item label="Email">{member.email}</Item>
         <Item label="Mobile Phone">{member.mobile_phone}</Item>
         <Item label="Business Phone">{member.business_phone}</Item>
@@ -61,7 +103,8 @@ const MemberInfo: FunctionComponent<Props> = ({ editable, member }) => {
         <Item label="How We Can Help">{member.how_can_we_help}</Item>
         <Item label="Dream">{member.dream}</Item>
         <Item label="Notes">{member.notes}</Item>
-        <Item label="Keywords">{member.keywords_EN}</Item>
+        <Item label="Keywords (EN)">{member.keywords_EN}</Item>
+        <Item label="Keywords (FR)">{member.keywords_FR}</Item>
       </Descriptions>
     </Card>
   );
