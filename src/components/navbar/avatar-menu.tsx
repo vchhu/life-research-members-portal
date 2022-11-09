be0f293ca713f92fdfcd810947c0654610808bf7
Chris Hamilton <chris_h_27@hotmail.ca>
2022-11-09 05:25:43 -0500

    show if account is admin / member in avatar menu

--- src/components/navbar/avatar-menu.tsx
+++ src/components/navbar/avatar-menu.tsx
@@ -1,21 +1,42 @@
 import { useMsal } from "@azure/msal-react";
 import Dropdown from "antd/lib/dropdown";
-import type { FC } from "react";
+import { FC, useContext } from "react";
 import Typography from "antd/lib/typography";
 import Card from "antd/lib/card";
 import LogoutButton from "./logout-button";
+import { ActiveAccountCtx } from "../../services/context/active-account-ctx";
+import { LanguageCtx } from "../../services/context/language-ctx";
+import CheckCircleTwoTone from "@ant-design/icons/lib/icons/CheckCircleTwoTone";
 
 const AvatarMenu: FC = () => {
-  const { instance } = useMsal();
-  const account = instance.getActiveAccount();
+  const { en } = useContext(LanguageCtx);
+  const { localAccount } = useContext(ActiveAccountCtx);
 
-  const avatarLabel = account?.name?.split(" ").reduce((prev, curr) => prev + curr[0], "");
+  if (!localAccount) return null;
+
+  const avatarLabel = localAccount.first_name[0] + localAccount.last_name[0];
+  const name = localAccount.first_name + " " + localAccount.last_name;
+  const email = localAccount.login_email;
+
+  const administrator = localAccount.is_admin ? (
+    <Typography>
+      {en ? "Administrator" : "Administrateur"} &nbsp; <CheckCircleTwoTone />
+    </Typography>
+  ) : null;
+
+  const member = localAccount.member ? (
+    <Typography>
+      {en ? "Member" : "Membre"} &nbsp; <CheckCircleTwoTone />
+    </Typography>
+  ) : null;
 
   const dropdown = (
     <Card bodyStyle={{ padding: 0 }}>
       <div className="avatar-dropdown">
-        <Typography>{account?.name}</Typography>
-        <Typography>{account?.username}</Typography>
+        <Typography>{name}</Typography>
+        <Typography>{email}</Typography>
+        {administrator}
+        {member}
         <div style={{ height: 16 }}></div>
         <LogoutButton />
       </div>
