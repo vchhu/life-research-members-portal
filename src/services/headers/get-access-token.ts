23281d2d84c0241df73b6d55aca2d6f22f8aaa3c
Chris Hamilton <chris_h_27@hotmail.ca>
2022-11-09 00:11:19 -0500

    Catch interaction required error and redirect to sign in

--- src/services/headers/get-access-token.ts
+++ src/services/headers/get-access-token.ts
@@ -1,4 +1,7 @@
 import { msalInstance, scopes } from "../../../auth-config";
+import { InteractionRequiredAuthError } from "@azure/msal-common";
+
+// See https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/token-lifetimes.md
 
 export default function getAccessToken() {
   return msalInstance
@@ -6,5 +9,10 @@ export default function getAccessToken() {
       scopes,
       account: msalInstance.getActiveAccount() || undefined,
     })
-    .then((res) => res.accessToken);
+    .then((res) => res.accessToken)
+    .catch((e) => {
+      if (e instanceof InteractionRequiredAuthError) {
+        return msalInstance.acquireTokenRedirect({ scopes });
+      }
+    });
 }
