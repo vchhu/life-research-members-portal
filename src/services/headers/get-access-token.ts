c00e57575b10d4574f7846c32c0e6e66821243a1
Chris Hamilton <chris_h_27@hotmail.ca>
2022-11-15 12:07:28 -0500

    Fix session expired errors

--- src/services/headers/get-access-token.ts
+++ src/services/headers/get-access-token.ts
@@ -1,5 +1,6 @@
 import { msalInstance, scopes } from "../../../auth-config";
 import { InteractionRequiredAuthError } from "@azure/msal-common";
+import Notification from "../notifications/notification";
 
 // See https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/token-lifetimes.md
 
@@ -10,9 +11,9 @@ export default function getAccessToken() {
       account: msalInstance.getActiveAccount() || undefined,
     })
     .then((res) => res.accessToken)
-    .catch((e) => {
-      if (e instanceof InteractionRequiredAuthError) {
-        return msalInstance.acquireTokenRedirect({ scopes });
-      }
+    .catch(() => {
+      // Just assume any error means session has expired
+      new Notification().error("Your session has expired, please login again.");
+      msalInstance.setActiveAccount(null);
     });
 }
