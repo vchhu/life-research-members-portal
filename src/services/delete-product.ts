8218c1060219e87114a0a3288499fe53c7b357d2
lmerc037 <lmerc037@uottawa.ca>
2023-03-26 23:04:15 -0400

    delete first implementation

---
+++ src/services/delete-product.ts
@@ -0,0 +1,25 @@
+import type { PrivateProductRes } from "../pages/api/product/[id]/private";
+import ApiRoutes from "../routing/api-routes";
+import { en } from "./context/language-ctx";
+import getAuthHeader from "./headers/auth-header";
+import Notification from "./notifications/notification";
+
+export default async function deleteProduct(id: number): Promise<PrivateProductRes | null> {
+  const authHeader = await getAuthHeader();
+  if (!authHeader) return null;
+
+  const notification = new Notification();
+  try {
+    notification.loading(en ? "Deleting Product..." : "Suppression du produit...");
+    const res = await fetch(ApiRoutes.deleteProduct(id), {
+      headers: authHeader,
+      method: "DELETE",
+    });
+    if (!res.ok) throw await res.text();
+    notification.success();
+    return await res.json();
+  } catch (e: any) {
+    notification.error(e);
+    return null;
+  }
+}
