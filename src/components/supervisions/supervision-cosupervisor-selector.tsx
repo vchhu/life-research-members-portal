e83ae938025289abea710e4a9de548b799c3c295
Daniel Amyot <damyot@site.uottawa.ca>
2023-04-17 16:15:37 -0400

    Fixed French translation

--- src/components/supervisions/supervision-cosupervisor-selector.tsx
+++ src/components/supervisions/supervision-cosupervisor-selector.tsx
@@ -68,12 +68,12 @@ const SupervisionCoSupervisorSelector: FC<Props> = ({
           <AutoComplete
             id={id}
             className="autocomplete"
-            placeholder={en ? "Search Existing..." : "Rechercher existant..."}
+            placeholder={en ? "Search Existing..." : "Rechercher..."}
             options={options}
             value={selectedValue}
             onChange={setSelectedValue}
             filterOption
-            notFoundContent={en ? "No Matches" : "Pas de Correspondance"}
+            notFoundContent={en ? "No Matches" : "Pas de correspondance"}
             onSelect={onSelect}
             onSearch={setSearchValue}
           />
